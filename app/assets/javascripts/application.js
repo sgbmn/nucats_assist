d58ffc282423b4e9677536f6a130ed8ad51e9d3e
blackjk3 <blackjk@gmail.com>
2015-01-27 13:20:59 -0600

    Missing jqueryUI for some reason.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery-ui
+// require jquery-ui
 //= require jquery.tablesorter.min
 //= require bootstrap-sprockets
 // require jquery_noconflict
